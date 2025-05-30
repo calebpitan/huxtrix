1fe1f1b7761121b496be7776492589de77a065c8
Caleb Adepitan <calebpitan@gmail.com>
2025-05-30 14:23:54 +0100

    chore(style): add rule for unused variables in TypeScript
    - Introduced a new ESLint rule to enforce no unused variables, allowing for ignored variables that start with an underscore.
    - Updated the ESLint configuration to enhance TypeScript code quality and maintainability.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,23 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 })
 
-const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript')]
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+]
 
 export default eslintConfig
