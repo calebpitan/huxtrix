105eca1542ce7dabb4ccb554a4593a31b6a21d1d
Caleb Adepitan <calebpitan@gmail.com>
2025-05-30 15:00:50 +0100

    feat: enhance AppFooter with responsive variants and improved styling
    - Refactored AppFooter to utilize class-variance-authority for managing responsive styles.
    - Introduced new variants for component, breakpoint, and strategy to enhance layout flexibility.
    - Updated AppFooterProps to include variant props for better customization.

--- src/compositions/footer/AppFooter.tsx
+++ src/compositions/footer/AppFooter.tsx
@@ -1,20 +1,88 @@
-import { HTMLAttributes } from 'react'
+import { ComponentProps } from 'react'
+
+import { VariantProps, cva } from 'class-variance-authority'
 
 import { cn } from '@/lib/utils'
 
-export interface AppFooterProps extends HTMLAttributes<HTMLDivElement> {
+type AppFooterStyleProps = VariantProps<typeof variants>
+
+export interface AppFooterProps
+  extends ComponentProps<'div'>,
+    Pick<AppFooterStyleProps, 'breakpoint' | 'strategy'> {
   className?: string
 }
 
-export const AppFooter = ({ className, ...props }: AppFooterProps) => {
+const variants = cva('', {
+  variants: {
+    component: {
+      footer: 'border-t-0 py-6 md:py-3',
+      content: 'flex flex-col items-center justify-between gap-4 md:flex-row',
+      paragraph: 'text-muted-foreground w-full text-center text-sm leading-loose',
+    },
+    breakpoint: { sm: '', md: '', lg: '', xl: '' },
+    strategy: { include: '', exclude: '' },
+  },
+  compoundVariants: [
+    {
+      component: 'footer',
+      breakpoint: 'sm',
+      strategy: 'include',
+      className: 'hidden sm:[display:revert] lg:hidden',
+    },
+    {
+      component: 'footer',
+      breakpoint: 'sm',
+      strategy: 'exclude',
+      className: 'sm:hidden md:[display:revert]',
+    },
+    {
+      component: 'footer',
+      breakpoint: 'md',
+      strategy: 'include',
+      className: 'hidden md:[display:revert] lg:hidden',
+    },
+    {
+      component: 'footer',
+      breakpoint: 'md',
+      strategy: 'exclude',
+      className: 'md:hidden lg:[display:revert]',
+    },
+    {
+      component: 'footer',
+      breakpoint: 'lg',
+      strategy: 'include',
+      className: 'hidden lg:[display:revert] xl:hidden',
+    },
+    {
+      component: 'footer',
+      breakpoint: 'lg',
+      strategy: 'exclude',
+      className: 'lg:hidden xl:[display:revert]',
+    },
+    {
+      component: 'footer',
+      breakpoint: 'xl',
+      strategy: 'include',
+      className: 'hidden xl:[display:revert]',
+    },
+    {
+      component: 'footer',
+      breakpoint: 'xl',
+      strategy: 'exclude',
+      className: 'xl:hidden',
+    },
+  ],
+})
+
+export const AppFooter = ({ className, breakpoint, strategy, ...props }: AppFooterProps) => {
   return (
     <div
       data-component="app-footer"
-      className={cn('border-t-0 py-6 md:py-3', className)}
+      className={cn(variants({ component: 'footer', breakpoint, strategy }), className)}
       {...props}
     >
-      <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
-        <p className="text-muted-foreground w-full text-center text-sm leading-loose">
+      <div className={variants({ component: 'content' })}>
+        <p className={variants({ component: 'paragraph' })}>
           <span>&copy;</span> 2025 HUXTRIX
         </p>
       </div>
