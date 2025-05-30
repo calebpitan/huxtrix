a8fb87afd00d93eefed7cb9ed0a4dc435637eb33
Caleb Adepitan <calebpitan@gmail.com>
2025-05-30 15:00:32 +0100

    feat: enhance navigation items and introduce dynamic user handling
    - Updated navigation items to use React components for icons, improving rendering consistency.
    - Renamed 'Submit' to 'Create' in navigation for clarity.
    - Added `getNavItems` function to dynamically generate navigation items based on user state and current page URL, enhancing user experience.

--- src/compositions/data/nav-items.tsx
+++ src/compositions/data/nav-items.tsx
@@ -1,34 +1,56 @@
 import { Compass, Home, PlusCircle, Search, User } from 'lucide-react'
 
+type GetNavItemsOptions = { pageUrl: URL | null; defaultItems?: Array<NavItem> }
+
+export type NavItem = (typeof navItems)[number]
+
 export const navItems = [
   {
     href: '/',
     label: 'Home',
-    icon: Home,
+    icon: <Home />,
     aria: 'Home',
   },
   {
     href: '/explore',
     label: 'Explore',
-    icon: Compass,
+    icon: <Compass />,
     aria: 'Explore',
   },
   {
-    href: '/submit',
+    href: '/create',
     label: 'Create',
-    icon: PlusCircle,
+    icon: <PlusCircle />,
     aria: 'Create new hack',
   },
   {
     href: '/search',
     label: 'Search',
-    icon: Search,
+    icon: <Search />,
     aria: 'Search',
   },
   {
     href: '/signin',
     label: 'Profile',
-    icon: User,
+    icon: <User />,
     aria: 'Profile',
   },
-]
+] as const
+
+export function getNavItems<U extends Record<'username', string>>(
+  user: U | undefined,
+  options: GetNavItemsOptions,
+) {
+  const { pageUrl, defaultItems = navItems } = options
+
+  const items = defaultItems
+    .filter((item) => item.label !== 'Profile' || user !== undefined)
+    .map((item) => {
+      const href = item.label !== 'Profile' ? item.href : `/${user!.username}`
+      const isActive = href === pageUrl?.pathname
+
+      return { ...item, href, isActive }
+    })
+
+  return items
+}
