add8add1f0d7a90ecf6c937458450696706c7546
Caleb Adepitan <calebpitan@gmail.com>
2025-07-02 10:35:29 +0100

    update components

---
+++ src/components/editor/plugins/list-classic-kit.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import {
+  BulletedListPlugin,
+  ListItemPlugin,
+  ListPlugin,
+  NumberedListPlugin,
+} from '@platejs/list-classic/react';
+
+import {
+  BulletedListElement,
+  NumberedListElement,
+} from '@/components/ui/list-classic-node';
+
+export const ListKit = [
+  ListPlugin,
+  BulletedListPlugin.configure({
+    node: { component: BulletedListElement },
+    shortcuts: { toggle: { keys: 'mod+alt+5' } },
+  }),
+  NumberedListPlugin.configure({
+    node: { component: NumberedListElement },
+    shortcuts: { toggle: { keys: 'mod+alt+6' } },
+  }),
+  ListItemPlugin,
+];
