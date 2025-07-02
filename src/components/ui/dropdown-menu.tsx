4eb991eaf73b77c1640cd14bb8179f37d3400276
Caleb Adepitan <calebpitan@gmail.com>
2025-07-02 10:49:06 +0100

    customize component styling

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -63,7 +63,7 @@ function DropdownMenuItem({
       data-inset={inset}
       data-variant={variant}
       className={cn(
-        "supports-[backdrop-filter]:focus:bg-primary/70 focus:bg-accent focus:text-accent-foreground data-[variant=destructive]:text-destructive data-[variant=destructive]:focus:bg-destructive/10 dark:data-[variant=destructive]:focus:bg-destructive/20 data-[variant=destructive]:focus:text-destructive data-[variant=destructive]:*:[svg]:!text-destructive [&_svg:not([class*='text-'])]:text-muted-foreground relative flex cursor-default items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 data-[inset]:pl-8 supports-[backdrop-filter]:focus:text-white supports-[backdrop-filter]:dark:focus:text-black [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
+        "supports-[backdrop-filter]:focus:bg-primary/70 focus:bg-accent focus:text-accent-foreground data-[variant=destructive]:text-destructive data-[variant=destructive]:focus:bg-destructive/10 dark:data-[variant=destructive]:focus:bg-destructive/20 data-[variant=destructive]:focus:text-destructive data-[variant=destructive]:*:[svg]:!text-destructive [&_svg:not([class*='text-'])]:text-muted-foreground supports-[backdrop-filter]:focus:[&_svg:not([class*='text-'])]:text-muted relative flex cursor-default items-center gap-2 rounded-sm px-2 py-1.5 text-sm outline-hidden select-none data-[disabled]:pointer-events-none data-[disabled]:opacity-50 data-[inset]:pl-8 supports-[backdrop-filter]:focus:text-white supports-[backdrop-filter]:dark:focus:text-black [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4",
         className,
       )}
       {...props}
--- src/components/ui/highlight-node.tsx
+++ src/components/ui/highlight-node.tsx
@@ -8,7 +8,7 @@ import { PlateLeaf } from 'platejs/react';
 
 export function HighlightLeaf(props: PlateLeafProps) {
   return (
-    <PlateLeaf {...props} as="mark" className="bg-highlight/30 text-inherit">
+    <PlateLeaf {...props} as="mark" className="bg-highlight/30 dark:bg-highlight/90 dark:text-black dark:caret-current text-inherit rounded-[2.5px] px-0.5">
       {props.children}
     </PlateLeaf>
   );
