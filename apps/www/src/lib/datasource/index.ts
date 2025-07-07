8682e236045e774d431d92ed08e70789fdb78bc0
Caleb Adepitan <calebpitan@gmail.com>
2025-07-07 22:49:10 +0100

    refactor: use environment to set logging

--- apps/www/src/lib/datasource/index.ts
+++ apps/www/src/lib/datasource/index.ts
@@ -2,6 +2,6 @@ import 'server-only'
 
 import { datasource } from '@hux/datasource'
 
-import { DATASOURCE_URL } from '@/lib/config/server'
+import { DATASOURCE_URL, NODE_ENV } from '@/lib/config/server'
 
-export const database = datasource(DATASOURCE_URL, { logger: true })
+export const database = datasource(DATASOURCE_URL, { logger: NODE_ENV === 'development' })
