1398d116e578fa05c564681316ff6043851c6214
Caleb Adepitan <calebpitan@gmail.com>
2025-07-08 02:07:38 +0100

    feat: wrap layout in session provider context

--- apps/www/src/compositions/layout/Root.tsx
+++ apps/www/src/compositions/layout/Root.tsx
@@ -3,6 +3,7 @@ import { NavigationGuardProvider } from 'next-navigation-guard'
 import { SidebarProvider } from '@/components/ui/sidebar'
 import { AppBarPassthrough, AppBarProvider, AppBarSlot } from '@/compositions/appbar'
 import { ThemeProvider } from '@/compositions/providers'
+import { SessionProvider } from '@/lib/auth/client'
 
 type RootLayoutProps = Readonly<{
   children: React.ReactNode
@@ -12,30 +13,32 @@ export function RootLayout({ children }: RootLayoutProps) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
       <NavigationGuardProvider>
-        <AppBarProvider>
-          <div data-component="root-layout" className="relative flex min-h-dvh flex-col">
-            <AppBarPassthrough slot="topbar">
-              <AppBarSlot slot="topbar" className="flex-1 lg:hidden" />
-            </AppBarPassthrough>
+        <SessionProvider>
+          <AppBarProvider>
+            <div data-component="root-layout" className="relative flex min-h-dvh flex-col">
+              <AppBarPassthrough slot="topbar">
+                <AppBarSlot slot="topbar" className="flex-1 lg:hidden" />
+              </AppBarPassthrough>
 
-            <div className="flex w-full flex-1 flex-col">
-              <SidebarProvider className="min-h-[calc(100dvh_-_var(--topbar-height,_var(--appbar-size-base)))] w-full flex-1 lg:min-h-full">
-                {children}
-              </SidebarProvider>
+              <div className="flex w-full flex-1 flex-col">
+                <SidebarProvider className="min-h-[calc(100dvh_-_var(--topbar-height,_var(--appbar-size-base)))] w-full flex-1 lg:min-h-full">
+                  {children}
+                </SidebarProvider>
+              </div>
+
+              <AppBarPassthrough slot="bottombar">
+                {/* <BreakpointPassthrough breakpoints="lg" strategy="exclude"> */}
+                <AppBarSlot
+                  slot="bottombar"
+                  data-breakpoint-excluded="lg"
+                  breakpoint="lg"
+                  strategy="exclude"
+                />
+                {/* </BreakpointPassthrough> */}
+              </AppBarPassthrough>
             </div>
-
-            <AppBarPassthrough slot="bottombar">
-              {/* <BreakpointPassthrough breakpoints="lg" strategy="exclude"> */}
-              <AppBarSlot
-                slot="bottombar"
-                data-breakpoint-excluded="lg"
-                breakpoint="lg"
-                strategy="exclude"
-              />
-              {/* </BreakpointPassthrough> */}
-            </AppBarPassthrough>
-          </div>
-        </AppBarProvider>
+          </AppBarProvider>
+        </SessionProvider>
       </NavigationGuardProvider>
     </ThemeProvider>
   )
