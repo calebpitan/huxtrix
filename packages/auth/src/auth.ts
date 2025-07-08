202bec6f090e28cccdf8f7c6bcafc4ff2bf5d8c5
Caleb Adepitan <calebpitan@gmail.com>
2025-07-08 02:11:04 +0100

    update authentication logic to distinguish sign-in from sign-up

--- packages/auth/src/auth.ts
+++ packages/auth/src/auth.ts
@@ -5,10 +5,11 @@ import {
   SessionModel,
   UserModel,
   VerificationTokenModel,
+  sql,
 } from '@hux/datasource'
 
 import NextAuth from 'next-auth'
-import type { NextAuthResult } from 'next-auth'
+import type { NextAuthConfig, NextAuthResult } from 'next-auth'
 import { Adapter } from 'next-auth/adapters'
 import Google, { GoogleProfile } from 'next-auth/providers/google'
 import Resend from 'next-auth/providers/resend'
@@ -18,12 +19,19 @@ interface HxAuthResult extends NextAuthResult {
 }
 
 type InitAuthEmailOptions = { serverAddress: string }
-type InitAuthOptions = { email: InitAuthEmailOptions; debug?: boolean }
+
+export type InitAuthOptions = Pick<NextAuthConfig, 'pages' | 'debug'> & {
+  email: InitAuthEmailOptions
+  sessionStrategy?: 'jwt' | 'database'
+  getIntent(): Promise<AuthIntent | undefined>
+  getBaseUrl(): Promise<string>
+}
 
 export type Auth = NextAuthResult['auth']
+export type AuthIntent = 'signin' | 'signup'
 
-function createAdapter(db: Database): Adapter {
-  const adapter = DrizzleAdapter(db, {
+function createAdapter(session: Database): Adapter {
+  const adapter = DrizzleAdapter(session, {
     accountsTable: AccountModel,
     usersTable: UserModel,
     sessionsTable: SessionModel,
@@ -33,18 +41,23 @@ function createAdapter(db: Database): Adapter {
   return adapter
 }
 
+export const AuthErrorCodes = {
+  acoountNotFound: 'account_not_found',
+  invalidEmail: 'invlaid_email',
+} as const
+
 /**
  * Initialize an authentication object that can be used to
  * setup authentication endpoints, sign in, and sign out from the server
  *
  * **Note:** These methods are only safe to be called from the server
  *
- * @param db The `PostgresJsDatabase` instance
+ * @param session The `PostgresJsDatabase` session instance
  * @returns The initialized authentication object
  */
-export function initAuth(db: Database, options: InitAuthOptions): HxAuthResult {
+export function initAuth(session: Database, options: InitAuthOptions): HxAuthResult {
   const result = NextAuth({
-    adapter: createAdapter(db),
+    adapter: createAdapter(session),
     providers: [
       Google<GoogleProfile>({
         profile(profile) {
@@ -60,6 +73,47 @@ export function initAuth(db: Database, options: InitAuthOptions): HxAuthResult {
       Resend({ from: options.email.serverAddress }),
     ],
     debug: options.debug,
+    pages: options.pages,
+    session: { strategy: options.sessionStrategy ?? 'jwt' },
+    callbacks: {
+      async signIn({ user, email: emailOptions }) {
+        const email = user.email?.trim()
+        const pathname = options.pages?.signIn ?? '/api/auth/signin'
+        const [intent, origin] = await Promise.all([
+          await options.getIntent(),
+          options.getBaseUrl(),
+        ])
+
+        if (!intent || intent === 'signup') {
+          return true
+        }
+
+        // TODO: validate the email
+        if (!email) {
+          const query = new URLSearchParams({ error: AuthErrorCodes.invalidEmail })
+          return `${origin}${pathname}?${query.toString()}`
+        }
+
+        const [{ exists }] = await session.execute<Record<'exists', boolean>>(sql`
+          SELECT EXISTS (
+            SELECT 1 
+              FROM ${UserModel}
+              WHERE ${UserModel.email} = ${email}
+              AND ${UserModel.deletedAt} IS NULL
+          )
+        `)
+
+        if (!exists) {
+          const query = new URLSearchParams({
+            error: AuthErrorCodes.acoountNotFound,
+            identifier: email,
+          })
+          return `${origin}${pathname}?${query.toString()}`
+        }
+
+        return true
+      },
+    },
   })
 
   return result
